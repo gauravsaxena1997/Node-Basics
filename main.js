4660ea93fa024f22e1538de2d7a91b8a3f5d8a22
gauravsaxena1997 <gauravsaxena.cs@gmail.com>
2019-07-08 13:45:37 +0530

    events

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // let http = require('http');
-let fs = require('fs');
+// let fs = require('fs');
+let events = require('events');
 
 // http.createServer( function(req,res){
 //     res.writeHead(200,{'Content-Type': 'text/plain'});
@@ -8,15 +9,27 @@ let fs = require('fs');
 
 // console.log('listening to port 8081...');
 
-// Read data synchronously (blocking)
+// Read data synchronously (blocking) ------------
 // let data = fs.readFileSync('input.txt');
 // console.log(data.toString());
 // console.log('Program Ends');
 
-// Read data asynchronously (non blocking)
-fs.readFile('input.txt', (err,data)=>{
-    if (err) return console.log(err);
-    console.log(data.toString());
-});
-console.log('Program Ends');
+// Read data asynchronously (non blocking) ------
+// fs.readFile('input.txt', (err,data)=>{
+//     if (err) return console.log(err.stack);
+//     console.log(data.toString());
+// });
+// console.log('Program Ends');
 
+// Events --------------
+// eventEmitter = new events.EventEmitter();
+// connectHandler = () => {
+//     console.log('Connection Successful...');
+//     eventEmitter.emit('data_received');
+// }
+// eventEmitter.on('connection',connectHandler);
+// eventEmitter.on('data_received',()=>{
+//     console.log('Data received.');   
+// });
+// eventEmitter.emit('connection');
+// console.log('Program ended.');
