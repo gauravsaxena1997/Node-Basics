ed2c97c9872bd31845c5d11f86aac011e0a97b58
gauravsaxena1997 <gauravsaxena.cs@gmail.com>
2019-07-08 13:18:34 +0530

    callbacks

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-let http = require('http');
-// http.createServer=((req,res)=>  {
-//     res.writeHead(200,{'Content-type': 'plain/text'});
+// let http = require('http');
+let fs = require('fs');
+
+// http.createServer( function(req,res){
+//     res.writeHead(200,{'Content-Type': 'text/plain'});
 //     res.end('Hello');
-// }).listen(8081)
+// } ).listen(8081);
 
-http.createServer( function(req,res){
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end('Hello');
-} ).listen(8081);
+// console.log('listening to port 8081...');
 
-console.log('listening to port 8081...');
+// Read data synchronously (blocking)
+// let data = fs.readFileSync('input.txt');
+// console.log(data.toString());
+// console.log('Program Ends');
+
+// Read data asynchronously (non blocking)
+fs.readFile('input.txt', (err,data)=>{
+    if (err) return console.log(err);
+    console.log(data.toString());
+});
+console.log('Program Ends');
 
